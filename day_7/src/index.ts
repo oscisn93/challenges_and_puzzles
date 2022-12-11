a6c4c48f8175ab9f6a1c78df0978d5a6f6d4c871
Oscar <ocisneros1@csu.fullerton.edu>
2022-12-11 01:28:55 -0800

    comments and formatting

--- day_7/src/index.ts
+++ day_7/src/index.ts
@@ -4,39 +4,43 @@ const input: Array<Array<string>> = readFileSync("input.txt", "utf-8")
   .split("\n")
   .map((line) => line.split(" "));
 
-function getDirectorySizes() {
+function getDirectorySizes(): number[] {
   let path: string[] = [];
-  const sizes = new Map<string, number> ();
+  const sizes = new Map<string, number>();
   for (let i = 0; i < input.length; i++) {
     const line = input[i];
     // if the line is a cd command
     if (line[1] === "cd") {
       // update the path (stack)
       let dir = line[2];
-      if (dir === "/") {
-        path = [];
-      } else if (dir === "..") {
-        path.pop();
-      } else {
-        path.push(dir);
-      }
+      // if the path is root empty the path
+      if (dir === "/") path = [];
+      // revert by popping the stack
+      else if (dir === "..") path.pop();
+      // otherwise push the new directory onto the stack
+      else path.push(dir);
     } else {
       // otherwise it must be an ls command
       do {
         i++;
+        // ignore directories
         if (input[i][0] === "dir") continue;
+        // must be a file, therefore, size = file size
         const size = Number(input[i][0]);
-        for (let end = path.length; end >=0; end--) {
+        // update all parent directories
+        for (let end = path.length; end >= 0; end--) {
+          // on each iteration the path shrinks
           const full_path = path.slice(0, end).join("/");
-          if (!sizes.has(full_path)) {
-            sizes.set(full_path, size);
-          } else {
+          // if the path does not yet exist update the map
+          if (!sizes.has(full_path)) sizes.set(full_path, size);
+          // other wise update the existing path
+          else {
             let d_size = sizes.get(full_path);
-            d_size += size;
-            sizes.set(full_path, d_size);
+            sizes.set(full_path, d_size + size);
           }
         }
-      } while (input[i+1] && input[i+1][0] != "$");
+        // continue until the next line contains a command
+      } while (input[i + 1] && input[i + 1][0] != "$");
     }
   }
   return [...sizes.values()];
@@ -44,7 +48,7 @@ function getDirectorySizes() {
 
 function firstPart() {
   const sizes = getDirectorySizes();
-  console.log(sizes.filter((n) => n <= 100000).reduce((a,b) => a+b));
+  console.log(sizes.filter((n) => n <= 100000).reduce((a, b) => a + b));
 }
 
 firstPart();
@@ -52,9 +56,9 @@ firstPart();
 function secondPart() {
   const sizes = getDirectorySizes();
   const used = Math.max(...sizes);
-  const free = 70000000-used;
+  const free = 70000000 - used;
   const required = 30000000 - free;
-  console.log(Math.min(...sizes.filter(n => n >= required)));
+  console.log(Math.min(...sizes.filter((n) => n >= required)));
 }
 
 secondPart();
